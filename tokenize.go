4a379035205b98b428ce4db14cd1f0dc9c583420
Jeff Lee <jeffomatic@gmail.com>
2017-04-30 17:55:21 -0700

    simplify tokenize case statement

--- tokenize.go
+++ tokenize.go
@@ -15,22 +15,11 @@ func tokenize(src string) []string {
 
 	for _, r := range src {
 		switch r {
-
-		case '(':
-			fallthrough
-		case ')':
+		case '(', ')':
 			appendCurrent()
 			res = append(res, string(r))
-
-		case ' ':
-			fallthrough
-		case '\t':
-			fallthrough
-		case '\n':
-			fallthrough
-		case '\r':
+		case ' ', '\t', '\n', '\r':
 			appendCurrent()
-
 		default:
 			current += string(r)
 		}
